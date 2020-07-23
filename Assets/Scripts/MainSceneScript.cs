2a1dc9c908f774baba8540f3ad2397360d00b660
alecphobia95 <46802114+alecphobia95@users.noreply.github.com>
2020-07-23 03:52:19 -0400

    UpdatedFolderPath

---
+++ Assets/Scripts/MainSceneScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSceneScript : MonoBehaviour
+{
+    public GameObject[] panels;
+    private int panelPos = 0;
+
+    private void Start()
+    {
+        ClearPanels();
+
+    }
+
+    private void ClearPanels()
+    {
+        for (int i = 0; i < panels.Length; i++)
+        {
+            panels[i].SetActive(false);
+        }
+        panels[panelPos].SetActive(true);
+    }
+}
