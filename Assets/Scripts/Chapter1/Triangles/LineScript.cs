4811f5364f4ac5f5eba6fc4c8cdb7427499a41ec
alecphobia95 <46802114+alecphobia95@users.noreply.github.com>
2021-07-01 13:17:21 -0400

    unknown

---
+++ Assets/Scripts/Chapter1/Triangles/LineScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineScript : MonoBehaviour
+{
+    public LineRenderer lr;
+
+    private void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+}
