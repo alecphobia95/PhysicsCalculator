10207ba5027fb2695c3ee42ab586fa3ffc051418
alecphobia95 <46802114+alecphobia95@users.noreply.github.com>
2021-08-25 00:56:24 -0400

    Added more stuff
    Added some more buttons and fields

--- Assets/Scripts/Chapter1/Triangles/LineScript.cs
+++ Assets/Scripts/Chapter1/Triangles/LineScript.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LineScript : MonoBehaviour
 {
     public LineRenderer lr;
-
-    private void Start()
-    {
-        lr = GetComponent<LineRenderer>();
-    }
+    public InputField adjacentInput, oppositeInput, hypotenuseInput, thetaInput;
+    [HideInInspector]
+    public float adjacentFloat;
+    [HideInInspector]
+    public float oppositeFloat;
+    [HideInInspector]
+    public float hypotenuseFloat;
+    [HideInInspector]
+    public float thetaFloat;
 
 }
